e558a2ec80b7ac06d781760dee5be6d60c5bd6b7
hector <hectorpoleo2007@gmail.com>
2025-03-30 22:23:51 +0100

    Add files via upload

---
+++ fate/fate/src/main/java/es/ies/puerto/fate/FateApplication.java
@@ -0,0 +1,13 @@
+package es.ies.puerto.fate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FateApplication.class, args);
+	}
+
+}
---
+++ fate/fate/src/test/java/es/ies/puerto/fate/FateApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ies.puerto.fate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
